abb60029fa4d90a8e9b2fb940d18c2ed8f55b836
Antriksh Yadav <Antrikshy@gmail.com>
2014-09-22 10:17:42 +0700

    Used callbacks to 'return' data, almost done

--- main.js
+++ main.js
@@ -4,33 +4,33 @@ var endpoints = {
     'director': "http://netflixroulette.net/api/api.php?director="
 };
 
-var titleUndefinedError = new Error("title must be defined");
-var titleNotStringError = new TypeError("title must be of type string");
-var yearNotNumberError = new TypeError("year must be of type number");
+var titleUndefinedError = new Error("'title' must be defined");
+var titleNotStringError = new TypeError("'title' must be of type string");
+var yearNotNumberError = new TypeError("'year' must be of type number");
 
-var actorUndefinedError = new Error("actor must be defined");
-var actorNotStringError = new TypeError("actor must be of type string");
+var actorUndefinedError = new Error("'actor' must be defined");
+var actorNotStringError = new TypeError("'actor' must be of type string");
 
-var directorUndefinedError = new Error("director must be defined");
-var directorNotStringError = new TypeError("director must be of type string");
+var directorUndefinedError = new Error("'director' must be defined");
+var directorNotStringError = new TypeError("'director' must be of type string");
 
 // API functions (entry-points of this module)
 module.exports = {
-    title: function(title, year) {
+    title: function(title, parentCallback, year) {
         // Error check: title must be defined
         if (title === undefined) {
             throw titleUndefinedError;
-            return null;
+            return;
         }
 
         // Error check: title must be string
         if (typeof title !== 'string') {
             throw titleNotStringError;
-            return null;
+            return;
         }
 
         // Encode title for request URL
-        encodedTitle = encodeCallParameters(title);
+        encodedTitle = require('querystring').escape(title);
         // Create final NetflixRoulette request URL
         requestURL = endpoints.title + encodedTitle;
 
@@ -39,60 +39,68 @@ module.exports = {
             // Error check: year must be number
             if (typeof year !== 'number') {
                 throw yearNotNumberError;
-                return null;
+                return;
             }
 
             // Append year parameter to URL
             requestURL = requestURL + '&year=' + parseInt(year, 10);
         }
 
-        return requestURL;
+        getJSON(requestURL, function(error, data) {
+            parentCallback(error, data);
+        });
     },
 
-    actor: function(actor) {
+    actor: function(actor, parentCallback) {
         // Error check: actor must be defined
         if (actor === undefined) {
             throw actorUndefinedError;
-            return null;
+            return;
         }
 
         // Error check: actor must be string
         if (typeof actor !== 'string') {
             throw actorNotStringError;
-            return null;
+            return;
         }
 
         // Encode actor for request URL
-        encodedActor = encodeCallParameters(actor);
+        encodedActor = require('querystring').escape(actor);
         // Create final NetflixRoulette request URL
         requestURL = endpoints.actor + encodedActor;
 
-        return requestURL;
+        getJSON(requestURL);
     },
 
-    director: function(director) {
+    director: function(director, parentCallback) {
         // Error check: director must be defined
         if (director === undefined) {
             throw directorUndefinedError;
-            return null;
+            return;
         }
 
         // Error check: director must be string
         if (typeof director !== 'string') {
             throw directorNotStringError;
-            return null;
+            return;
         }
 
         // Encode director for request URL
-        encodedDirector = encodeCallParameters(director);
+        encodedDirector = require('querystring').escape(director);
         // Create final NetflixRoulette request URL
         requestURL = endpoints.director + encodedDirector;
 
-        return requestURL;
+        getJSON(requestURL);
     }
 };
 
-function encodeCallParameters(searchString) {
-    var querystring = require('querystring');
-    return querystring.escape(searchString);
+function getJSON(requestURL, callback) {
+    var request = require('request');
+        console.log(requestURL)
+        request({
+            url: requestURL,
+            json: true
+        }, function(error, response, data) {
+            callback(error, data);
+        });
 }
