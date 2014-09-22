c708460d77ebf24af04a757c9df15e9e2be8177e
Antriksh Yadav <Antrikshy@gmail.com>
2014-09-22 11:06:56 +0700

    Cleaned up excessively defensive error checking

--- main.js
+++ main.js
@@ -4,28 +4,25 @@ var endpoints = {
     'director': "http://netflixroulette.net/api/api.php?director="
 };
 
-var titleUndefinedError = new Error("'title' must be defined");
-var titleNotStringError = new TypeError("'title' must be of type string");
-var yearNotNumberError = new TypeError("'year' must be of type number");
+var titleNotStringError = new TypeError("'title' must be of type string.");
+var yearNotNumberError = new TypeError("'year' must be of type number.");
+var actorNotStringError = new TypeError("'actor' must be of type string.");
+var directorNotStringError = new TypeError("'director' must be of type string.");
 
-var actorUndefinedError = new Error("'actor' must be defined");
-var actorNotStringError = new TypeError("'actor' must be of type string");
-
-var directorUndefinedError = new Error("'director' must be defined");
-var directorNotStringError = new TypeError("'director' must be of type string");
+var callbackUndefinedError = new Error("Callback function must be defined.")
 
 // API functions (entry-points of this module)
 module.exports = {
     title: function(title, parentCallback, year) {
-        // Error check: title must be defined
-        if (title === undefined) {
-            throw titleUndefinedError;
-            return;
-        }
-
         // Error check: title must be string
         if (typeof title !== 'string') {
             throw titleNotStringError;
+            return;
+        }
+
+        // Error check: parentCallback must be defined
+        if (parentCallback == undefined || typeof parentCallback !== 'function') {
+            throw callbackUndefinedError;
             return;
         }
 
@@ -46,21 +43,22 @@ module.exports = {
             requestURL = requestURL + '&year=' + parseInt(year, 10);
         }
 
+        // Get JSON fron NetflixRoulette, pass data into parent's callback
         getJSON(requestURL, function(error, data) {
             parentCallback(error, data);
         });
     },
 
     actor: function(actor, parentCallback) {
-        // Error check: actor must be defined
-        if (actor === undefined) {
-            throw actorUndefinedError;
-            return;
-        }
-
         // Error check: actor must be string
         if (typeof actor !== 'string') {
             throw actorNotStringError;
+            return;
+        }
+
+        // Error check: parentCallback must be defined
+        if (parentCallback == undefined || typeof parentCallback !== 'function') {
+            throw callbackUndefinedError;
             return;
         }
 
@@ -69,21 +67,22 @@ module.exports = {
         // Create final NetflixRoulette request URL
         requestURL = endpoints.actor + encodedActor;
 
+        // Get JSON fron NetflixRoulette, pass data into parent's callback
         getJSON(requestURL, function(error, data) {
             parentCallback(error, data);
         });
     },
 
     director: function(director, parentCallback) {
-        // Error check: director must be defined
-        if (director === undefined) {
-            throw directorUndefinedError;
-            return;
-        }
-
         // Error check: director must be string
         if (typeof director !== 'string') {
             throw directorNotStringError;
+            return;
+        }
+
+        // Error check: parentCallback must be defined
+        if (parentCallback == undefined || typeof parentCallback !== 'function') {
+            throw callbackUndefinedError;
             return;
         }
 
@@ -92,18 +91,22 @@ module.exports = {
         // Create final NetflixRoulette request URL
         requestURL = endpoints.director + encodedDirector;
 
+        // Get JSON fron NetflixRoulette, pass data into parent's callback
         getJSON(requestURL, function(error, data) {
             parentCallback(error, data);
         });
     }
 };
 
+// Retreives JSON, uses passed in callback to 'return' data to caller-scope
 function getJSON(requestURL, callback) {
+    // Uses request library (from npm)
     var request = require('request');
         request({
             url: requestURL,
             json: true
         }, function(error, response, data) {
+            // Calls callback passed by one of the API functions
             callback(error, data);
         });
 }
